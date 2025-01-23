b9ca1c85f1aa79baf1a5aa7ce4ada6407adbacf1
Alexandr Vinokurov <salispiligrim@yandex.ru>
2025-01-23 19:27:41 +0500

    add added users

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const path = require("path");
 const fs = require("fs");
+const joi = require("joi");
 const handlebars = require("handlebars");
 const app = express();
 const port = 3000;
@@ -9,6 +10,8 @@ const templatePath = path.join(__dirname, "HTML/templates/users.handlebars");
 
 app.use(express.static(path.join(__dirname, "css")));
 app.use(express.static(path.join(__dirname, "HTML")));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "HTML/index.html"));
@@ -31,6 +34,42 @@ app.get("/users", (req, res) => {
                 res.send(html);
             }
         });
+    });
+});
+
+app.post("/submit", (req, res) => {
+    const userIn = req.body;
+    console.log("input user:\n" + userIn);
+    fs.readFile("users.json", "utf-8", (err, data) => {
+        if (err) {
+            res.status(500);
+            res.send(err.message);
+        } else {
+            const users = JSON.parse(data);
+            const hasName = users.some(
+                (user) => user.username === userIn.username
+            );
+            if (!hasName) {
+                users.push(userIn);
+                fs.writeFile(
+                    "users.json",
+                    JSON.stringify(users, null, 2),
+                    "utf-8",
+                    (writeErr) => {
+                        if (writeErr) {
+                            return res.status(500);
+                        }
+
+                        res.status(200);
+                        res.redirect("/");
+                    }
+                );
+            } else {
+                return res.status(400).send({
+                    error: "This user already exists",
+                });
+            }
+        }
     });
 });
 
